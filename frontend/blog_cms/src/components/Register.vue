1577d78346671b97ea6753f86c3e76a27973b901
Leonardo Arellano Martinez <leo.aremtz98@gmail.com>
2021-01-17 02:11:26 -0600

    Estructura del frontend creada

---
+++ frontend/blog_cms/src/components/Register.vue
@@ -0,0 +1,55 @@
+<template>
+	<div class="register">
+		<p>register</p>
+		<font-awesome-icon icon="user-secret" />
+		<el-form ref="signUpData" :model="signUpData">
+			<el-form-item>
+				<el-input
+					placeholder="Email"
+					v-model="signUpData.email"
+					autofocus
+					clearable
+					autocomplete="off"
+				></el-input>
+			</el-form-item>
+
+			<el-form-item>
+				<el-input
+					placeholder="Password"
+					v-model="signUpData.password"
+					show-password
+					clearable
+					autocomplete="off"
+				></el-input>
+			</el-form-item>
+
+			<el-form-item>
+				<el-button type="primary" @click="submitForm('ruleForm')">
+					Sign up
+				</el-button>
+			</el-form-item>
+		</el-form>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Register',
+	data() {
+		return {
+			signUpData: {
+				email: '',
+				password: '',
+			},
+		}
+	},
+	methods: {
+		submitForm() {
+			console.log('submit!')
+			console.log(this.signUpData)
+		},
+	},
+}
+</script>
+
+<style></style>
---
+++ frontend/blog_cms/src/views/SignUp.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="signup">
+		<h1>Signup</h1>
+		<Register />
+	</div>
+</template>
+
+<script>
+import Register from '@/components/Register.vue'
+
+export default {
+	name: 'SignUp',
+	components: {
+		Register,
+	},
+}
+</script>
+
+<style></style>
